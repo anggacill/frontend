6831965f52bb1caa0ca99c0c7322adc5fd85df6b
anggacill <anggalahyakan@gmail.com>
2024-07-10 11:29:08 +0700

    saya menambahkan helo padqa app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Kotak1 from "./componen/Kotak1"
+import Kotak2 from "./componen/Kotak2"
 
 function App() {
   return (
     <>
-      <h1>Hello world</h1>
+      <h1>Hello world </h1>
       <Kotak1/>
       <Kotak2/>
     </>
